3ae421af3df4a0c0e85042b4c29c28d6df444592
listyantidewi1 <listyantidewi@gmail.com>
2025-01-20 11:25:55 +0700

    created two variables for start and end alignment of the gradient container

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_teach/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             const Color.fromARGB(255, 43, 24, 76),
             const Color.fromARGB(255, 97, 30, 109)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: Center(child: StyledText()),
