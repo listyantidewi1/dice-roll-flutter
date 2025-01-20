69a28eff36acf89376fc407bbb89dad022cbae9e
listyantidewi1 <listyantidewi@gmail.com>
2025-01-20 11:05:01 +0700

    moved entire container into GradientContainer class

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 43, 24, 76),
-                const Color.fromARGB(255, 97, 30, 109)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28.5,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 43, 24, 76),
+            const Color.fromARGB(255, 97, 30, 109)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.5,
+          ),
+        ),
+      ),
+    );
+  }
 }
