a76eaed43598426c2f78318ac11a5a7ae0499d4f
listyantidewi1 <listyantidewi@gmail.com>
2025-01-20 09:35:08 +0700

    added BackgroundColor in Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 35, 18, 63),
         body: Center(
           child: Text('Hello World!'),
         ),
