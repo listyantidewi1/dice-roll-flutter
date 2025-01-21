77253a55b1b6334dd7cd715f67f0a5113ec3fd4a
listyantidewi1 <listyantidewi@gmail.com>
2025-01-21 08:35:30 +0700

    added text style to button text

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,9 +9,7 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice(){
-    
-  }
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -25,6 +23,7 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
@@ -32,6 +31,10 @@ class GradientContainer extends StatelessWidget {
             ),
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(fontSize: 28),
+              ),
               child: const Text('Roll Dice!'),
             )
           ],
